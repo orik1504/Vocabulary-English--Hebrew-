6d055b4330dcb3389af3d0f01b8a7b71210381d8
orik1504 <orik1504@gmail.com>
2021-01-04 19:22:10 +0200

    implomented the database and class

--- Vocabulary.py
+++ Vocabulary.py
@@ -6,32 +6,29 @@ import atexit
 
 
 
-# -- function that dumps value to json file -- #
-def write_in_json (translate , file ="Vocabulary/Vocabulary-English--Hebrew-/values.json"):
-    with open ("Vocabulary/Vocabulary-English--Hebrew-/values.json", "w") as new_file:
-        json.dump(translate,new_file)
+class Vocabulary():
+    def __init__(self , json_path):
+        self.__json_path = json_path
+    def add_to_database(self,word, translation):
+        with open(self.__json_path) as f:
+            data = json.load(f)
+        data[word] = translation
+        with open(self.__json_path, 'w', encoding = 'utf8') as f:
+            json.dump(data, f , indent = 4)
 
-# -- open the file and into the "words" list and append the value that I gave -- #
-with open ("Vocabulary/Vocabulary-English--Hebrew-/values.json") as file:
-    data = json.load(file) #what I already saved (dict "words" have value of list)
-    in_data = data['words'] ##entering to "words" -  list of words in json fil
-    value = {"paper":"1"} #need to make another value every time
-    in_data.append(value) #append value to "words" list
+    def morfix (self):
+        
+        
+        self.add_to_database(w,t)
+
+v = Vocabulary("values.json")    
+
+w, t = input("enter word:"), input("enter translation:") #need to add morfix values
+
+
+
+
+
+
     
 
-print (in_data)
-
-# # # # # # # # # #
-# -- C H E C K -- #
-# # # # # # # # # #
-
-# -- get values to json file -- #
-# vlaue = "orange"
-# with open("Vocabulary/Vocabulary-English--Hebrew-/values.json",'w', encoding = 'utf8') as new_file: #open json file to write into as file
-#   in_file = file['words']
-#   in_file.append(vlaue)
-
-# with open("Vocabulary/Vocabulary-English--Hebrew-/values.json") as file:
-#     data = json.load(file) # dict "words" have value of list
-#     in_data = data['words'] #entering to "words" -  list of words in json file 
-# print (type(data))
