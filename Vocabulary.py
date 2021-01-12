c5e445b902c1b75ab83022527ca986f9c34bed9b
orik1504 <orik1504@gmail.com>
2021-01-12 20:02:27 +0200

    ✨added WordCollection

--- Vocabulary.py
+++ Vocabulary.py
@@ -1,67 +1,87 @@
-import json
-import datetime
-from bidi.algorithm import get_display 
-from selenium import webdriver
+from api import WordTranslationGenerator
+import random
+import os
+import pickle
 import atexit
-from bs4 import BeautifulSoup
-import requests
-import spellchecker
 
-class Vocabulary():
+class WordCollection():
     
-    def __init__(self, json_path):
-        self.__json_path = json_path
-    
-    def add_to_database(self,word, translation):
-        with open(self.__json_path) as f:
-            data = json.load(f)
-        data[word] = translation
-        with open(self.__json_path, 'w', encoding='utf8') as f:
-            json.dump(data, f, indent=4)
+    def __init__(self,):
+        self.__words = list()
 
+    def add(self, word, translation):
+        self.__words.append((word,translation))
+    
+    def add_random(self,):
+        word,translation =  WordTranslationGenerator.word_translation()
+        self.add(word, translation)
+    
+    def get_last(self,):
+        if not self.__words:
+            return None
+        return self.__words[-1]
+        
+    
+    def get_random(self,):
+        if not self.__words:
+            return None
+        return random.choice(self.__words)
+    
+class Database():
+
+    
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.data = self.__load()
+        atexit.register(self.__save)
+
+    def __load (self,):
+        if os.path.isfile(self.file_name):
+            with open (self.file_name, "rb") as file:
+                return pickle.load(file)
+
+        return WordCollection()
+    
+    def __save (self,): 
+        with open (self.file_name, "wb") as file:
+            pickle.dump(self.data, file)
+        
+    
+    
+    
+class Dictations():
     def print_out(self):
-    """ Prints out the value of the last in json """
+        """ Prints out the value of the last in json """
 
     def dictation(self):
-    """ prints last 15 values in saturday for dictation """
+        """ prints last 15 values in saturday for dictation """
+        
 
     def check_input (self):
-    """ Checks input of user """
+        """ Checks input of user """
+
 
     def right(self):
-    """ Prints if all answers are right """
-        print (" You got them all right !")
+        """ Prints if all answers are right """
+
     
     def wrong(self):
-    """ Prints that you had mistakes in dictation """
-        print("You had {} mistakes")
+        """ Prints that you had mistakes in dictation """
+        
     
     def try_again(self):
-    """ Make a new try for the same dictatin """
+        """ Make a new try for the same dictatin """
+
 
     def where_mistake(self):
-    """ prints where the mistakes are """
+        """ prints where the mistakes are """
 
 
-class morfix ():
-    
-    self.where_database = Vocabulary.add_to_database()
-   
-    def __init__ (self):
-        pass
 
-    def request(self):
-    """ Return morfix file string """
 
-    def morfix (self):
-    """ takes values from responce """
-        # Add stuff
-        self.add_to_database(w,t)
-    
 
-v = Vocabulary("values.json")    
 
-w, t = input("enter word:"), input("enter translation:") 
 
 
 
